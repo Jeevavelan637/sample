67d45c867041bc036c290edb245a8c7886240be3
Jeevavelan637 <jeevavelan2005@gmail.com>
2025-01-30 18:09:03 +0530

    Add files via upload

---
+++ led.ino
@@ -0,0 +1,33 @@
+
+void setup() {
+ pinMode(2,OUTPUT);
+ pinMode(7,OUTPUT);
+ pinMode(8,OUTPUT);
+ pinMode(9,OUTPUT);
+ pinMode(10,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ digitalWrite(2,HIGH);
+ delay(5);
+ digitalWrite(2,LOW);
+ delay(2);
+ digitalWrite(7,HIGH);
+ delay(5);
+ digitalWrite(7,LOW);
+ delay(2);
+ digitalWrite(8,HIGH);
+ delay(5);
+ digitalWrite(8,LOW);
+ delay(2);
+ digitalWrite(9,HIGH);
+ delay(5);
+ digitalWrite(9,LOW);
+ delay(2);
+ digitalWrite(10,HIGH);
+ delay(5);
+ digitalWrite(10,LOW);
+ delay(2                            );
+ 
+}
